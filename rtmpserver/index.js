8c92f621be3af967902e31cf669abc69cf1e2ec2
thembones79 <michal.szumnarski@gmail.com>
2019-12-07 16:56:33 +0100

    added media server

--- rtmpserver/index.js
+++ rtmpserver/index.js
@@ -1,18 +1,18 @@
-const NodeMediaServer = require("node-media-server");
+const NodeMediaServer = require('node-media-server');
 
 const config = {
-  rtmp: {
+rtmp: {
     port: 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60
-  },
-  http: {
+},
+http: {
     port: 8000,
-    allow_origin: "*"
-  }
+    allow_origin: '*'
+}
 };
 
-var nms = new NodeMediaServer(config);
+var nms = new NodeMediaServer(config)
 nms.run();
